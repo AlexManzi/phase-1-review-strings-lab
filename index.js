597f800cdad087f1361124f889ba1a43dc5e1a05
Alex Manzi <alex.manzi22@gmail.com>
2022-02-07 13:14:54 -0500

    commit tree 7db8fdd18fc0ae1d1104a135cc53acd74e0e1c36

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Write your code in this file!
-let currentUser = 'Alex';
-let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+let currentUser = "Alex"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
